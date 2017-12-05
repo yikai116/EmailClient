00ed82a4f71d096613a9f6dceae4c637c9e2767e
yikai <1164323901@qq.com>
2017-12-05 14:06:23 +0800

    主界面

---
+++ app/src/main/java/com/exercise/p/emailclient/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.exercise.p.emailclient.activity;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.exercise.p.emailclient.GlobalInfo;
+import com.exercise.p.emailclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences preferences = getSharedPreferences("Info",MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("token", GlobalInfo.user.getAccessToken());
+        editor.apply();
+    }
+}
